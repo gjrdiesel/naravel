53356f6a8b681e149311cb4e18b5fe7d22e5190f
Justin <knoxxbox@gmail.com>
2016-08-22 23:37:44 -0400

    Added and slightly working sessions

--- deluminate/bootstrap.js
+++ deluminate/bootstrap.js
@@ -1,10 +1,11 @@
 module.exports = function(dirname){
-	var express = require('express');
-
-	var bodyParser = require('body-parser');
+	var express = require('express'),
+		bodyParser = require('body-parser'),
+		cookieParser = require('cookie-parser'),
+    	session = require('express-session');
 	
 	var app = express();
-	var models = require(dirname + '/http/models');
+	var db = require(dirname + '/http/models');
 	var exphbs  = require('express-handlebars');
 
 	app.engine('.hbs', exphbs({
@@ -14,15 +15,28 @@ module.exports = function(dirname){
 		partialsDir: dirname + '/resources/views/partials'
 	}));
 	app.set('view engine', '.hbs');
-	app.set('views', dirname + '/resources/views');  
+	app.set('views', dirname + '/resources/views');
+
+	var SequelizeStore = require('connect-sequelize')(session),
+    modelName = 'Session',
+    options = {
+        // our session options if any. see @https://www.npmjs.com/package/connect-sequelize
+    };
 	
+	app.use(cookieParser())
+	app.use(session({
+	  secret: 'keyboard cat',
+	  store: new SequelizeStore(db.sequelize, options, modelName),
+	  proxy: true // if you do SSL outside of node. 
+	}));
+
 	app.use(bodyParser.urlencoded({ extended: false }))
 
 	app.use(express.static('public/'));
 
 	require(dirname + '/http/routes.js')(app);
 
-	models.sequelize.sync().then(function () {
+	db.sequelize.sync().then(function () {
 		app.listen(3000, function () {
 	  		console.log('listening at http://localhost:3000!');
 		});
